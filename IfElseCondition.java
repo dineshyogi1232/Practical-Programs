9380435e513b87daed143a4b9ad34715def4a227
tej9697 <tej9697@gmail.com>
2022-02-01 20:32:54 +0530

    Changes done

--- IfElseCondition.java
+++ IfElseCondition.java
@@ -3,19 +3,20 @@ package com.Programs;
 import java.util.Scanner;
 
 /**
- * @author	Dinesh 
- * @desc	age check using ifelse condiion
+ * @author	Dinesh Yogi 
+ * @desc	Age check using if else condition
  */
 public class IfElseCondition {
 
 	public static void main(String[] args) {
-		final int MINIMUMAGE = 18;
+		final int minimumAge = 18;
 		int age = 0;
 		Scanner scannerObj = new Scanner(System.in);
 		System.out.println("Enter the Age: ");
 		age = scannerObj.nextInt();
 
-		if (age >= MINIMUMAGE) {
+		//for check age
+		if (age >= minimumAge) {
 			System.out.println("You are eligible for vote");
 		} else {
 			System.out.println("You are not eligible for vote");
