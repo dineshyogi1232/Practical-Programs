123063deb11542f6bb7f93cadf7733d70725b61f
tej9697 <tej9697@gmail.com>
2022-02-01 19:00:38 +0530

    Changes has been done

--- Demo.java
+++ Demo.java
@@ -1,11 +1,13 @@
 package com.Programs;
 
 /**
- * @author Dinesh 
- * @desc Hello world program
+ * @author	Dinesh 
+ * @desc	Hello world program
  */
 public class Demo {
+
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 	}
+
 }
