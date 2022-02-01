fc24233f00388813018012df02a9737ec39bd11c
tej9697 <tej9697@gmail.com>
2022-02-01 17:54:07 +0530

    Hello world committed

---
+++ Demo.java
@@ -0,0 +1,11 @@
+package com.Programs;
+
+/**
+ * @author Dinesh 
+ * @desc Hello world program
+ */
+public class Demo {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
