0a754dfc0aac68718239973aaa3154c6250d44ad
tej9697 <tej9697@gmail.com>
2022-02-01 19:12:36 +0530

    Changes has been done

---
+++ WrapperClasses.java
@@ -0,0 +1,18 @@
+package com.Programs;
+
+/**
+ * @author	Dinesh
+ * @desc	Compare two variables of Wrapper class using Wrapper classes
+ * 
+ */
+public class WrapperClasses {
+
+	public static void main(String[] args) {
+		Integer wf = 0;
+		Integer ws = 0;
+
+		System.out.println(wf.compareTo(ws));
+		System.out.println(ws.compareTo(wf));
+	}
+
+}
