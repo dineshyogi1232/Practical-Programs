ee2cf0e775b0445d6d91523ee48fd0e6d06a9063
tej9697 <tej9697@gmail.com>
2022-02-01 18:17:19 +0530

    EMI Calculator using Class, Object, Variables, Operators, and Datatypes.

---
+++ Operators.java
@@ -0,0 +1,32 @@
+package com.Programs;
+
+import java.util.Scanner;
+
+/**
+ * @author	Dinesh Yogi
+ * @desc	Apply EMI Calculator in Java - Java program to calculate EMI using
+ *		Class, Object, Variables, Operators, and Datatypes
+ */
+public class Operators {
+
+	public static void main(String[] args) {
+		int p = 0;
+		float r = 0;
+		int n = 0;
+		double EMI = 0.0;
+		Scanner scObj = new Scanner(System.in);
+		System.out.println("Enter the Principal loan amount you wish to avail (rupees): ");
+		p = scObj.nextInt();
+		System.out.println("Enter the Rate of interest (percentage): ");
+		r = scObj.nextFloat();
+		System.out.println("Enter the Loan term (months):");
+		n = scObj.nextInt();
+		r = r / 12 / 100;
+
+		EMI = (p * r * Math.pow((1+r), n)) / ((Math.pow((1+r), n) - 1));
+
+		System.out.println("EMI is :" + EMI);
+		scObj.close();
+	}
+
+}
