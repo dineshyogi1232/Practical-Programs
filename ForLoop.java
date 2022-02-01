c9cfb1c86ad66ae3e7d2fdc83378114aa8f8936d
tej9697 <tej9697@gmail.com>
2022-02-01 18:16:18 +0530

    Pattern using for loop

---
+++ ForLoop.java
@@ -0,0 +1,21 @@
+package com.Programs;
+
+/**
+ * @author	Dinesh 
+ * @desc	Pattern print using forloop
+ */
+public class ForLoop {
+	public static void main(String[] args) {
+		int i=0;
+		int j=0;
+		int n=5;
+
+		for(i=1;i<=n;i++) {
+			for(j=1;j<=i;j++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+	}
+	
+}
