3b4e508f9a4b848256ac5d422b91ed38ae0012e0
tej9697 <tej9697@gmail.com>
2022-02-02 19:42:56 +0530

    Changes done

--- StringMethods.java
+++ StringMethods.java
@@ -1,10 +1,7 @@
 package com.Programs;
-
 /**
- * 
- * @author	Dinesh Yogi
- * @des		All the string methods like charAt(), concat(), equals() etc.
- *
+ * @author Dinesh Yogi
+ * @des All the string methods like charAt(), concat(), equals() etc.
  */
 public class StringMethods {
 
@@ -15,18 +12,17 @@ public class StringMethods {
 		System.out.println("Concating two strings: " + string1.concat(string2));
 		System.out.println("Comparing two objects of string using equals method: " + string1.equals(string2));
 		System.out.println("Comparing two objects of string using compareto method: " + string1.compareTo(string2));
-		System.out.println("Length Of String: "+ string1.length());
-		System.out.println("Sub String from string: "+ string1.substring(2));
-		System.out.println("Sub String from String: "+ string2.substring(2,7));
-		System.out.println("Check String empty or Not: "+ string1.isEmpty());
-		System.out.println("Check String blank or Not: "+ string1.isBlank());
+		System.out.println("Length Of String: " + string1.length());
+		System.out.println("Sub String from string: " + string1.substring(2));
+		System.out.println("Sub String from String: " + string2.substring(2, 7));
+		System.out.println("Check String empty or Not: " + string1.isEmpty());
+		System.out.println("Check String blank or Not: " + string1.isBlank());
 		System.out.println("Replace the character: " + string1.replace("H", "h"));
 		System.out.println("Return Interned String: " + string2.intern());
-		System.out.println("Return char index: " + string1.indexOf('e') );
+		System.out.println("Return char index: " + string1.indexOf('e'));
 		System.out.println("String Convert into lowercase:" + string2.toLowerCase());
 		System.out.println("String Convert into uppercase:" + string1.toUpperCase());
 		System.out.println("Remove all spaces from string :" + string2.trim());
-
 
 	}
 
