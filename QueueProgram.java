2aebcad93fd331ef9b98cd03ecc86c957e00c7ef
tej9697 <tej9697@gmail.com>
2022-02-03 13:08:32 +0530

    Queue Program

---
+++ QueueProgram.java
@@ -0,0 +1,35 @@
+package com.Collection;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * @author	Dinesh Yogi
+ * @desc	Demonstrate of Queue Program
+ */
+public class QueueProgram {
+
+	public static void main(String[] args) {
+		int size = 10;
+		Queue<Integer> queue = new LinkedList<Integer>();
+		for (int count = 0; count <= size; count++) {
+			queue.add(count);
+		}
+		System.out.println(queue);
+		/*
+		 * remove() & poll() difference is if queue will be empty then remove() throws
+		 * exception but poll() returns null
+		 */
+		System.out.println("Remove element from head using remove()... " + queue.remove());
+		System.out.println("Remove element from head using poll()... " + queue.poll());
+		System.out.println(queue);
+		System.out.println("Getting element from head using peek()... " + queue.peek());
+		System.out.println("Getting element from head using element()... " + queue.element());
+		System.out.println(queue);
+		System.out.println("Check if queue is empty or not... " + queue.isEmpty());
+		System.out.println("Check the size of queue... " + queue.size());
+		System.out.println("It will be add element from tail... " + queue.offer(11));
+		System.out.println(queue);
+	}
+
+}
